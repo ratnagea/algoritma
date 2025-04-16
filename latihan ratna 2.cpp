0720ff105dee0d49d2518185d392054d8e5106ef
ratnagea <ratnawatigea68@gmail.com>
2025-04-16 19:58:35 +0700

    Add files via upload

---
+++ latihan ratna 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+const float pi = 3.14;
+float r = 7.0;
+float luas;
+
+luas = pi * (r * r);
+
+cout << "luas lingkaran dengan jari-jari" << r << "cm adalah:" << luas << "cm²" << endl;
+
+return 0;
+}
+
